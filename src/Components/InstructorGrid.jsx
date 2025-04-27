c2a6029ba359f43e160e162a3a3b866ef1a6a2fd
Iram Saba Khan <161953870+iramsk02@users.noreply.github.com>
2025-04-27 08:33:04 +0530

    Update InstructorGrid.jsx

--- src/Components/InstructorGrid.jsx
+++ src/Components/InstructorGrid.jsx
@@ -3,7 +3,6 @@
 import InstructorCard from "./instructorCard";
 
 export default function InstructorGrid({ educators }) {
-    console.log(educators[0].name)
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 text-black">
             {educators.map((educator) => (
